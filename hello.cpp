fc8546001a0bfbbf753038ebceee72dde58bcb97
vshreyasivani <113627381+vshreyasivani@users.noreply.github.com>
2025-03-11 14:04:56 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hi CC LAB\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
